8257d0438bc42c6eea517397460f659d689584f3
Jaronpoel <jaron.poel@gmail.com>
2020-10-09 16:51:00 +0200

    Sprint 2

---
+++ WedstrijdSysteem/WedstrijdSysteem/Controllers/ReportController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WedstrijdSysteem.Controllers
+{
+    public class ReportController : Controller
+    {
+        public IActionResult Oversight()
+        {
+            return View();
+        }
+        public IActionResult SingleReport()
+        {
+            return View();
+        }
+        public IActionResult NewReport()
+        {
+            return View();
+        }
+    }
+}
---
+++ WedstrijdSysteem/WedstrijdSysteem/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WedstrijdSysteem.Controllers
+{
+    public class UserController : Controller
+    {
+        public ActionResult SignIn()
+        {
+            return View();
+        }
+
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+
+        public ActionResult Info()
+        {
+            return View();
+        }
+    }
+}
